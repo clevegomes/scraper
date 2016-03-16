83bee07434e12c12ab0cdde7c8639aa9008dea17
Cleve <gomescleve@gmail.com>
2016-03-17 00:02:48 +0400

    Completed

--- fetchScraper.php
+++ fetchScraper.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once "init.php";
-require_once "/var/www/html/scraper/Cls/Scraper.php";
+//require_once "/var/www/html/scraper/Cls/Scraper.php";
 $searchString = $_GET["searchString"];
 $order = $_GET["order"];
 
 
-//$scraperObj = new Scraper($searchString);
+$scraperObj = new \Cls\Scraper($searchString);
 
 //$scraperObj->sorting($order);
 //echo $scraperObj->getItemArry();
--- init.php
+++ init.php
@@ -3,6 +3,10 @@ spl_autoload_register(function($class){
 
 //    print_r("$class.php");exit;
 	//require_once "$class.php";
+
+    $class = str_replace('\\', '/', $class);
+    if(file_exists('/var/www/html/scraper/' . $class . '.php'))
+        require_once('/var/www/html/scraper/' . $class . '.php');
 });
 
 
