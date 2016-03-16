29776ae498b9e70641fbd9c72b69dbf2f60b9d1b
Cleve <gomescleve@gmail.com>
2016-03-16 19:38:19 +0400

    Completed

--- fetchScraper.php
+++ fetchScraper.php
@@ -3,12 +3,12 @@
 require_once "init.php";
 $searchString = $_GET["searchString"];
 $order = $_GET["order"];
-
+echo "test";
+exit;
 
 
 $scraperObj = new \Cls\Scraper($searchString);
-echo $searchString;
-exit;
+
 $scraperObj->sorting($order);
 echo $scraperObj->getItemArry();
 
