e03bcf30907bdc0a8d590728ed0936949948abc3
Cleve <gomescleve@gmail.com>
2016-03-17 00:03:54 +0400

    Completed

--- fetchScraper.php
+++ fetchScraper.php
@@ -6,7 +6,7 @@ $searchString = $_GET["searchString"];
 $order = $_GET["order"];
 
 
-$scraperObj = new \Cls\Scraper($searchString);
+$scraperObj = new \Cls\Scraperg($searchString);
 
 //$scraperObj->sorting($order);
 //echo $scraperObj->getItemArry();
