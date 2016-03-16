297e536b24257476d4cab5552ae117e97325fb3c
Cleve <gomescleve@gmail.com>
2016-03-16 23:48:45 +0400

    Completed

--- fetchScraper.php
+++ fetchScraper.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "init.php";
-require_once "Cls/Scraper.php";
+require_once "/var/www/html/scraper/Cls/Scraper.php";
 $searchString = $_GET["searchString"];
 $order = $_GET["order"];
 
